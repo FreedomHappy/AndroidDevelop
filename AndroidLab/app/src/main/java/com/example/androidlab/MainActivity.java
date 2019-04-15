f3ad951d9e2aaf1112b732bc68f347419bdb9405
lin <2437706590@qq.com>
2019-04-15 23:29:35 +0800

    preferences

--- AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.java
+++ AndroidLab/app/src/main/java/com/example/androidlab/MainActivity.java
@@ -86,4 +86,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ContextualMenuActivity.class);
         startActivity(intent);
     }
+    public void ToPreference(View view){
+        Intent intent = new Intent(this, PreferenceActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ AndroidLab/app/src/main/java/com/example/androidlab/PreferenceActivity.java
@@ -0,0 +1,54 @@
+package com.example.androidlab;
+
+
+
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.MenuItem;
+
+public class PreferenceActivity extends AppCompatActivity implements
+        PreferenceFragmentCompat.OnPreferenceStartFragmentCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new MySettingsFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
+        // Instantiate the new Fragment
+        final Bundle args = pref.getExtras();
+        final Fragment fragment = new newFragment();
+        fragment.setArguments(args);
+        fragment.setTargetFragment(caller, 0);
+        // Replace the existing Fragment with the new Fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.settings_container, fragment)
+                .addToBackStack(null)
+                .commit();
+        return true;
+    }
+    public static class MySettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preference, rootKey);
+        }
+    }
+    public static class newFragment extends PreferenceFragmentCompat{
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState,String rootKey){
+            setPreferencesFromResource(R.xml.newprefernce,rootKey);
+        }
+    }
+}
